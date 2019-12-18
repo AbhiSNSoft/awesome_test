161764cf68d9c442d004e44292381bb09041e508
Abhinay M <abhiany@snsoft.my>
2019-12-18 11:10:37 +0800

    Update appcenter-post-build.sh [build-d]

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -26,20 +26,20 @@ APP=$MGB_APP_NAME
 #
 # Contributed by Manigandan Balachandran
 #
-# This is to get the Build Details so we could pass it as part of the Email Body
-build_url=https://appcenter.ms/orgs/$ORG/apps/$APP/build/branches/$APPCENTER_BRANCH/builds/$APPCENTER_BUILD_ID
-# Address to send email
-TO_ADDRESS="abhinay@snsoft.my"
-# A sample Subject Title 
-SUBJECT="AppCenter Build"
-# Content of the Email on Build-Success.
-SUCCESS_BODY="Success! Your build($APPCENTER_BUILD_ID) completed successfully!\n\n"
-# Content of the Email on Build-Failure.
-FAILURE_BODY="Sorry! Your AppCenter Build($APPCENTER_BUILD_ID) failed. Please review the logs and try again.\n\n"
-#If Agent Job Build Status is successful, Send the email, if not send a failure email.``
 
 OS_ANDROID=android
 OS_IOS=ios
+
+# MGB_BUILD_MODE
+# 0 => Staging
+# 1 => Production (Default)
+MGB_BUILD_MODE=1
+
+# MGB_BUILD_UPLOAD
+# true => Upload build
+# false => Don't upload build (Default)
+MGB_BUILD_UPLOAD=false
+
 
 echo "BUILD_SOURCEVERSION: $BUILD_SOURCEVERSION"
 echo "APPCENTER_BUILD_ID: " $APPCENTER_BUILD_ID
@@ -52,51 +52,89 @@ echo "APPCENTER_XCODE_SCHEME: " $APPCENTER_XCODE_SCHEME
 echo "APPCENTER_ANDROID_VARIANT: " $APPCENTER_ANDROID_VARIANT
 echo "APPCENTER_ANDROID_MODULE: " $APPCENTER_ANDROID_MODULE
 echo "APPCENTER_REACTNATIVE_PACKAGE: " $APPCENTER_REACTNATIVE_PACKAGE
+echo "COMMIT_MESSAGE: $COMMIT_MESSAGE"
 
+# This is to get the Build Details so we could pass it as part of the Email Body
+build_url=https://appcenter.ms/users/$ORG/apps/$APP/build/branches/$APPCENTER_BRANCH/builds/$APPCENTER_BUILD_ID
+# Address to send email
+TO_ADDRESS="abhinay@snsoft.my"
+# A sample Subject Title 
+SUBJECT="AppCenter Build $AGENT_JOBSTATUS"
+# Content of the Email on Build-Success.
+SUCCESS_BODY="Success! Your build($APPCENTER_BUILD_ID) completed successfully!\n\n"
+# Content of the Email on Build-Failure.
+FAILURE_BODY="Sorry! Your AppCenter Build($APPCENTER_BUILD_ID) failed. Please review the logs and try again.\n\n"
+#If Agent Job Build Status is successful, Send the email, if not send a failure email.
 if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; 
     then
-    echo "OS detected: " $MGB_SYSTEM_OS
-    # ANDROID - upload .apk file to Manager
-    if [ "$MGB_SYSTEM_OS" == "$OS_ANDROID" ]; 
+    # Upload build to Manager if branch is production(rn0.6_prod)
+	if [ "$APPCENTER_BRANCH" == "rn0.6_prod" ];
         then
-        echo "$MGB_SYSTEM_OS uploading started"
-        curl \
-        -X POST https://mgb.snapplog.com/list/upload \
-        -H "content-type: multipart/form-data" \
-        -F "version=$MGB_APP_VERSION" \
-        -F "content=$COMMIT_MESSAGE -- uploaded from App Center($MGB_PLATFORM_NAME, Build id:$APPCENTER_BUILD_ID)" \
-        -F "mode=1" \
-        -F "system=0" \
-        -F "updateMode=0,1" \
-        -F "platformId=$MGB_PLATFORM_ID" \
-        -F "apk=@$APPCENTER_OUTPUT_DIRECTORY/app-$MGB_PLATFORM_NAME-release.apk" \
-        -F "token=$MGB_TOKEN"
-        echo "$MGB_SYSTEM_OS uploading finished"
-    fi
-    
-    # IOS - upload .ipa file to Manager
-    if [ "$MGB_SYSTEM_OS" == "$OS_IOS" ]; 
+        echo "production branch $APPCENTER_BRANCH"
+        $MGB_BUILD_UPLOAD=true
+        $MGB_BUILD_MODE=1
+    # Upload build to Manager if commit message contains [build-d]
+    elif [ "$COMMIT_MESSAGE" == *"[build-d]"* ]; 
         then
-        echo "$MGB_SYSTEM_OS uploading started"
-        curl \
-        -X POST https://mgb.snapplog.com/list/upload \
-        -H "content-type: multipart/form-data" \
-        -F "version=$MGB_APP_VERSION" \
-        -F "content=$COMMIT_MESSAGE -- uploaded from App Center($MGB_PLATFORM_NAME, Build id:$APPCENTER_BUILD_ID)" \
-        -F "mode=1" \
-        -F "system=1" \
-        -F "updateMode=0,1" \
-        -F "platformId=$MGB_PLATFORM_ID" \
-        -F "apk=@$APPCENTER_OUTPUT_DIRECTORY/$MGB_PLATFORM_NAME.ipa" \
-        -F "token=$MGB_TOKEN"
-        echo "$MGB_SYSTEM_OS uploading finished"
+        echo "build-d staging"
+        $MGB_BUILD_UPLOAD=true
+        $MGB_BUILD_MODE=0
+        # Upload build to Manager if commit message contains [build-p]
+    elif [ "$COMMIT_MESSAGE" == *"[build-p]"* ];
+        then
+        echo "build-d pre-release"
+        $MGB_BUILD_UPLOAD=true
+        $MGB_BUILD_MODE=1
     fi
 
+    echo "MGB_BUILD_UPLOAD: $MGB_BUILD_UPLOAD"
+    echo "MGB_BUILD_MODE: $MGB_BUILD_MODE"
+    if [ "$MGB_BUILD_UPLOAD" == "true" ]; 
+        then
+        echo "OS detected: " $MGB_SYSTEM_OS
+        # ANDROID - upload .apk file to Manager
+        if [ "$MGB_SYSTEM_OS" == "$OS_ANDROID" ]; 
+            then
+            echo "$MGB_SYSTEM_OS uploading started"
+            # curl \
+            # -X POST https://mgb.snapplog.com/list/upload \
+            # -H "content-type: multipart/form-data" \
+            # -F "version=$MGB_APP_VERSION" \
+            # -F "content=$COMMIT_MESSAGE -- uploaded from App Center($MGB_PLATFORM_NAME, Build id:$APPCENTER_BUILD_ID)" \
+            # -F "mode=$MGB_BUILD_MODE" \
+            # -F "system=0" \
+            # -F "updateMode=0,1" \
+            # -F "platformId=$MGB_PLATFORM_ID" \
+            # -F "apk=@$APPCENTER_OUTPUT_DIRECTORY/app-$MGB_PLATFORM_NAME-release.apk" \
+            # -F "token=$MGB_TOKEN"
+            echo "$MGB_SYSTEM_OS uploading finished"
+        fi
+        
+        # IOS - upload .ipa file to Manager
+        if [ "$MGB_SYSTEM_OS" == "$OS_IOS" ]; 
+            then
+            echo "$MGB_SYSTEM_OS uploading started"
+            # curl \
+            # -X POST https://mgb.snapplog.com/list/upload \
+            # -H "content-type: multipart/form-data" \
+            # -F "version=$MGB_APP_VERSION" \
+            # -F "content=$COMMIT_MESSAGE -- uploaded from App Center($MGB_PLATFORM_NAME, Build id:$APPCENTER_BUILD_ID)" \
+            # -F "mode=$MGB_BUILD_MODE" \
+            # -F "system=1" \
+            # -F "updateMode=0,1" \
+            # -F "platformId=$MGB_PLATFORM_ID" \
+            # -F "apk=@$APPCENTER_OUTPUT_DIRECTORY/$MGB_PLATFORM_NAME.ipa" \
+            # -F "token=$MGB_TOKEN"
+            echo "$MGB_SYSTEM_OS uploading finished"
+        fi
+    fi
     echo "Build Success!"
-    echo -e ${SUCCESS_BODY} ${build_url} | mail -s "$MGB_PLATFORM_NAME ${SUBJECT} - Success!" ${TO_ADDRESS}
+    echo -e ${SUCCESS_BODY} ${build_url} | mail -s "$MGB_PLATFORM_NAME $MGB_SYSTEM_OS($APPCENTER_BUILD_ID) ${SUBJECT}" ${TO_ADDRESS}
     echo "success mail sent"
+
+#If Agent Job Build Status is failed, Send the failure email.
 else
     echo "Build Failed!"
-    echo -e ${FAILURE_BODY} ${build_url} | mail -s "$MGB_PLATFORM_NAME ${SUBJECT} - Failed!" ${TO_ADDRESS}
+    echo -e ${FAILURE_BODY} ${build_url} | mail -s "$MGB_PLATFORM_NAME $MGB_SYSTEM_OS($APPCENTER_BUILD_ID) ${SUBJECT}" ${TO_ADDRESS}
     echo "failure mail sent"
 fi
